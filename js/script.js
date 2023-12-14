a4ab0dc69d6f7988338bff0eeb69669f8d0bc5ea
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-12-14 14:48:14 +0100

    add: API and created() axios call

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 const { createApp } = Vue;
 
 createApp({
+    data() {
+        return{
+            discsArr: [],
+            apiURL: "server.php",
+        };
+    },
+    methods: {
 
+    },
+    created() {
+        axios
+        .get(this.apiURL)
+        .then((resp) => {
+            console.log(resp.data);
+            const discs = resp.data
+            this.discsArr = discs
+            console.log("discsArr", this.discsArr);
+        })
+    },
 }).mount("#app");
